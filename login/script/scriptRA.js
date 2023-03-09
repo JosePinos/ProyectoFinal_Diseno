5af103cacfc881ff93fb6ad05108e7b533dee369
Jhowellz <jhoelpatino26@hotmail.com>
2023-03-09 13:50:13 -0500

    Funcionalidades - Agregada la mayoria de funcionalidades - Aun sin centrar el div XDDD

---
+++ login/script/scriptRA.js
@@ -0,0 +1,16 @@
+function messageFB()
+{
+    var correo = prompt("Usuario")
+    var pass = prompt("Contrasenia")
+    var mensaje = "Cuenta de Facebook vinculada correctamente!!"
+
+    alert(mensaje)
+}
+function messageIG()
+{
+    var correo = prompt("Usuario")
+    var pass = prompt("Contrasenia")
+    var mensaje = "Cuenta de Instagram vinculada correctamente!!"
+
+    alert(mensaje)
+}
