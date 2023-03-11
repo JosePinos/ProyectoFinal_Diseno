52d5a544016e92d4900e44d3c20a612984bc0ffa
Einarr07 <aidenperse@gmail.com>
2023-03-11 16:21:09 -0500

    administrador fin

---
+++ Administrador/index.js
@@ -0,0 +1,15 @@
+function abrir_aceptacion(){
+    document.getElementById("aceptacion").style.display="block";
+}
+
+function cerrar_aceptacion(){
+    document.getElementById("aceptacion").style.display="none";
+}
+
+function abrir_rechazo(){
+    document.getElementById("rechazo").style.display="block";
+}
+
+function cerrar_rechazo(){
+    document.getElementById("rechazo").style.display="none";
+}
